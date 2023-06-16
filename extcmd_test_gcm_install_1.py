032ab3e43adeca46ac61f514f3fc101733a2fca9
advancedvirtual <134952601+advancedvirtual@users.noreply.github.com>
2023-06-16 03:54:40 +0300

    Create extcmd_test_gcm_install_1.py

---
+++ extcmd_test_gcm_install_1.py
@@ -0,0 +1,16 @@
+
+class extcmd_handler_main():    
+    
+    td = None    
+    
+           
+    def __init__(self, ____td____):
+        self.td = ____td____
+        
+        
+    def testttseter_r(cwd:str="."):
+      print(100101)
+      
+    commands = [
+                  {"name": "testgcminstallresult_text", 'function': testttseter_r, 'neededArgs': False, 'description': ''}
+    ]  
